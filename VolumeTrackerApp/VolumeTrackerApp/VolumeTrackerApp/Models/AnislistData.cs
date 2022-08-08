87bfe3632ae3fdf50268fa9717bf6aa295d5ef9a
Breanna Henriquez <50420371+B-Hen@users.noreply.github.com>
2022-08-07 21:51:55 -0400

    Set up the project with a small test case

---
+++ VolumeTrackerApp/VolumeTrackerApp/VolumeTrackerApp/Models/AnislistData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VolumeTrackerApp.Models
+{
+    class AnislistData
+    {
+
+        public class Rootobject
+        {
+            public Data data { get; set; }
+        }
+
+        public class Data
+        {
+            public Media Media { get; set; }
+        }
+
+        public class Media
+        {
+            public int id { get; set; }
+            public object volumes { get; set; }
+            public Title title { get; set; }
+        }
+
+        public class Title
+        {
+            public string english { get; set; }
+        }
+
+    }
+}
---
+++ VolumeTrackerApp/VolumeTrackerApp/VolumeTrackerApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using VolumeTrackerApp.Models;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+using GraphQL.Client.Http;
+using GraphQL.Client.Abstractions.Websocket;
+using GraphQL;
+using GraphQL.Client.Serializer.Newtonsoft;
+
+namespace VolumeTrackerApp.ViewModels
+{
+class MainPageViewModel
+    {
+        //variables
+        public ObservableCollection<string> Data { get; set; }
+        public IGraphQLWebsocketJsonSerializer JsonSerializer { get; }
+
+        //commands
+        public ICommand SearchCommand { get; set; }
+
+        //constructor
+        public MainPageViewModel()
+        {
+            Data = new ObservableCollection<string>();
+            Data.Add("Test");
+
+            //search command which will take in the value of the search bar and use it to get info
+            //back from the anilist api
+            SearchCommand = new Command<string>(async(search) => 
+            {
+                Data.Add(search);
+                await GetAnilistData(search);
+            });
+        }
+
+        public void GetSearchValue(string search)
+        {
+            Data.Add(search);
+            
+        }
+
+        public async Task GetAnilistData(string search)
+        {
+            var graphQLClient = new GraphQLHttpClient("https://graphql.anilist.co/", new NewtonsoftJsonSerializer());
+
+            var request = new GraphQLHttpRequest
+            {
+                Query = "query{Media(search: \"chainsaw\"){id,volumes,title{english}}}"
+            };
+
+            var response = await graphQLClient.SendQueryAsync<AnislistData.Data>(request);
+            Data.Add(response.Data.Media.title.english);
+            /*var client = new HttpClient();
+   
+            var stringContent = new StringContent("query ($id: Int, $page: Int, $perPage: Int, $search: String, $type: MediaType) {Page (page: $page, perPage: $perPage) {pageInfo {total currentPage lastPage hasNextPage perPage} media (id: $id, search: chainsaw man, type: MANGA) {id title {romaji english native} volumes chapters coverImage {extraLarge large medium color}}}}");
+
+            var httpResponse = await client.PostAsync("https://graphql.anilist.co/", stringContent);
+
+            var json = await httpResponse.Content.ReadAsStringAsync();
+
+            var obj = JsonConvert.DeserializeObject<AnislistData.Rootobject>(json);*/
+        }
+    }
+}
